1401048bd990f5a0f18bacd8f7356c137f42d379
saebyul1221 <dw696969@naver.com>
2023-01-18 20:14:11 +0900

    ✨ 개발자 옵션 추가

---
+++ src/main/java/com/bank/DeveloperOption.java
@@ -0,0 +1,56 @@
+package com.bank;
+
+import com.bank.Console.ConsoleInput;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DeveloperOption {
+    private static ConsoleInput console = new ConsoleInput();
+    private static final String propFile = "config/config.properties";
+    private Connection conn = null;
+    private String password = null;
+
+    public DeveloperOption(Connection conn)  {
+        this.conn = conn;
+        configSetup();
+    }
+
+    public void configSetup() {
+        try {
+            Properties prop = new Properties();
+            FileInputStream fis = new FileInputStream(propFile);
+
+            prop.load(new java.io.BufferedInputStream(fis));
+            password = prop.getProperty("DEV_PASSWORD");
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+
+    public void transfer() {
+        console.log("입금 할 계좌를 입력해주세요.");
+        String account = console.next("> ");
+
+        console.log("입금 할 금액을 입력해주세요.");
+        String money = console.next("> ");
+
+        PreparedStatement stmt;
+        String insQuery = "UPDATE `Account` SET `Balance` = `Balance` + ? WHERE `Account` = ?";
+
+        try {
+            stmt = conn.prepareStatement(insQuery);
+            stmt.setInt(1, Integer.parseInt(money));
+            stmt.setString(2, account);
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
+
+    }
+}
