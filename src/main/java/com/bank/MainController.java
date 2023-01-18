76b5841787a0021f44773ef498c98ea1ebf2ae13
saebyul1221 <dw696969@naver.com>
2023-01-18 19:49:55 +0900

    📚 개선사항

--- src/main/java/com/bank/MainController.java
+++ src/main/java/com/bank/MainController.java
@@ -1,3 +1,10 @@
+/**
+ * @개선사항
+ *
+ * 1. 객체 파라미터로 넘겨주는 정보(BACK_PIN, username 등)을 DTO로 데이터를 주고 받도록 수정.
+ * 2. 어노테이션 적극 활용하기
+ * 3. 여러 디자인 패턴 활용하기
+ */
 package com.bank;
 
 import com.bank.Console.ConsoleManager;
